4a91870bfff7ac3b826f2aa6827c26d1e1e460a0
Surasak Sincharoen <moomdate@gmail.com>
2021-05-26 10:02:03 +0700

    Test todo

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,49 @@
 <script>
-	export let name;
-</script>
+    let value;
+    let inputField
+    let valueList = []
 
+
+    function handleBtn() {
+
+        valueList = [...valueList, value];
+        inputField.value = '';
+    }
+
+    function onDelete(id) {
+        valueList.splice(id, 1);
+        valueList = valueList;
+    }
+
+</script>
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <input type="text" bind:this={inputField} bind:value={value}>
+    <button on:click={handleBtn}>Add todo</button>
+    {#each valueList as value,i}
+        <p> {value}
+            <button style="color: red"  on:click={()=>onDelete(i)}>x</button>
+        </p>
+    {/each}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
